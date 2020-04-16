f5edecad4e8c4df67a01dc8fe2142fc10afc341b
kijaewon <61965038+kijaewon@users.noreply.github.com>
2020-04-16 17:14:16 +0900

    Add dotenv,KAKAO_API
    Add dotenv,KAKAO_API

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
+from bs4 import BeautifulSoup
+import requests
+import json
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+KAKAO_API_KEY = os.getenv('KAKAO_API_KEY')
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
 
 app = Flask(__name__)
 
@@ -8,7 +21,37 @@ def welcome():
 
 
 
-if __name__=='__main__' :
-    app.run('localhost', 5000, debug=True)
 
-    
+@app.route('/info',methods=['POST'])
+def info():
+   searchbook = request.form['searchbook']
+   search_key = request.form['search_key']
+   if search_key == 'title' :
+      url="https://dapi.kakao.com/v3/search/book?target=title"
+   elif search_key == 'authors':
+      url="https://dapi.kakao.com/v3/search/book?target=person"
+   elif search_key == 'ISBN' :
+      url="https://dapi.kakao.com/v3/search/book?target=isbn"
+   
+   headers={ 'Authorization' : KAKAO_API_KEY }
+   queryString={'query':searchbook}
+
+   data = requests.get(url,headers=headers,params=queryString)
+   soup = BeautifulSoup(data.text, 'html.parser')
+   books = json.loads(soup.text)
+
+   info=books['documents']
+
+      
+
+  # book={'title':title,'authors':authors,'contents':contents,'thumbnail':thumbnail}
+
+   #db.books.insert_one(book)
+
+   return jsonify({'result':'success','info':info})
+
+
+
+if __name__=='__main__' :
+    app.run('localhost', 5005, debug=True)
+
