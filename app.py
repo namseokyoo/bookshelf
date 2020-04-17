71b98b57b0099fc7077b65d5c04c74627d825f79
namseokyoo <namseok.yoo@gmail.com>
2020-04-17 18:38:42 +0900

    Add review.py
    api 에서 검색된 책의 url에 review Crawling

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from dotenv import load_dotenv
+
+from review import req_review
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
    return render_template('index.html')
+
+
+@app.route('/review')
+def review():
+    url='https://search.daum.net/search?w=bookpage&bookId=532462&q=%ED%94%BC%ED%94%84%ED%8B%B0+%ED%94%BC%ED%94%8C'
+    result = req_review(url)
+    return jsonify({'result':result})
 
 
 
---
+++ run.bat
@@ -0,0 +1,5 @@
+::#!/bin/bash
+set FLASK_APP=app.py
+set FLASK_ENV=development
+
+flask run --host 0.0.0.0
