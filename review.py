b1cd950c7bbbd5b79cf019c46e93b3e7e322f72e
namseokyoo <namseok.yoo@gmail.com>
2020-04-17 19:51:35 +0900

    Add test and review

--- review.py
+++ review.py
@@ -62,18 +62,5 @@ def req_review(product_url):
 
 
 
-# # movies (tr들) 의 반복문을 돌리기
-# rank = 1
-# for movie in movies:
-#     # movie 안에 a 가 있으면,
-#     a_tag = movie.select_one('td.title > div > a')
-#     if a_tag is not None:
-#         title = a_tag.text
-#         star = movie.select_one('td.point').text
-#         print(rank, title, star)
-#         db.movies.insert_one({'title': title , 'star': star , 'rank': rank})
-#         rank += 1
-
-
 
 
