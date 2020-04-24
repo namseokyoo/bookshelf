142191e0c61d571e7d173686dfa532fc071ea0e6
kijaewon <61965038+kijaewon@users.noreply.github.com>
2020-04-24 21:13:39 +0900

    Add bookinfo in calendar

--- app.py
+++ app.py
@@ -2,7 +2,11 @@
 import os
 import requests
 import json
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, make_response
+from uuid import uuid4
+from datetime import datetime, timedelta
+from flask.sessions import SessionInterface, SessionMixin
+from werkzeug.datastructures import CallbackDict
 
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
@@ -16,15 +20,86 @@ from searchvideo import search_video
 load_dotenv()
 KAKAO_API_KEY = os.getenv('KAKAO_API_KEY')
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+client = MongoClient('localhost', 27017)
+db = client.session
 
 app = Flask(__name__)
 
 
+class MongoSession(CallbackDict, SessionMixin):
+    def __init__(self, initial=None, sid=None):
+        CallbackDict.__init__(self, initial)
+        self.sid = sid
+        self.modified = False
+
+
+class MongoSessinoInterface(SessionInterface):
+    def __init__(self, host='localhost', port=27017,\
+                db='', collection='sessions'):
+        client = MongoClient(host, port)
+        self.store = client[db][collection]
+
+    def open_session(self, app, request):
+        sid = request.cookies.get(app.session_cookie_name)
+        if sid:
+            stored_session = self.store.find_one({'sid': sid})
+            if stored_session:
+                if stored_session.get('expiration') > datetime.utcnow():
+                    return MongoSession(initial=stored_session['books'],
+                                        sid=stored_session['sid'])
+        sid = str(uuid4())
+        return MongoSession(sid=sid)
+
+    def save_session(self, app, session, response):
+        domain = self.get_cookie_domain(app)
+        if session is None:
+            response.delete_cookie(app.session_cookie_name, domain=domain)
+            return
+        if  self.get_expiration_time(app, session):
+            expiration = self.get_expiration_time(app, session)
+        else:
+            expiration = datetime.utcnow() +  timedelta(hours=1)
+
+        self.store.update({'sid': session.sid}, {
+                            'sid': session.sid,
+                            'books': session,
+                            'expiration': expiration
+                        }, True)
+        response.set_cookie(app.session_cookie_name,
+                            session.sid,
+                            expires=self.get_expiration_time(app, session),
+                            httponly=True, domain=domain)
+
+
+app.session_interface = MongoSessinoInterface(db='session')
+app.config.update(
+    SESSION_COOKIE_NAME='flask_session'
+)
+
+
 @app.route('/')
 def welcome():
-    return render_template('index.html')
+    res = make_response(render_template('index.html'))
+    res.set_cookie(app.session_cookie_name, session.sid)
+    return res
+    
+
+
+@app.route('/session_in', methods=['GET','POST'])
+def session_signin():
+    title = request.args.get('title')
+    url = request.args.get('url')
+    print(session.sid)
+    session[title] = {'title':title, 'url':url}
+    print(session)
+    event = list(db.sessions.find({},{'_id':0}))
+    return jsonify({'result':'success', 'event':event})
+
+
+@app.route('/session_out')
+def session_signout():
+    session.clear()
+    return "Signout"
 
 
 @app.route('/reviews', methods=['POST'])
@@ -54,6 +129,15 @@ def video():
     result = search_video(options)
     return jsonify({'result': 'success', 'searchvideo': result})
 
+@app.route('/bookshelf')
+def bookshelf():
+   return render_template('fullcalendar.html')
+
+@app.route('/bookshelflist')
+def addShelf():
+    event = list(db.sessions.find({},{'_id':0}))
+    print(event)
+    return jsonify({'result':'success','event':event})
 
 @app.route('/info', methods=['POST'])
 def info():
